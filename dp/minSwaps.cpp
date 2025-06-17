6f2a3a7e5c29692f37b626dd87376f1d77c1eb00
tanmay bansal <121656091+TanmayBansa1@users.noreply.github.com>
2025-06-17 08:48:03 +0530

    equal partition sum

---
+++ dp/minSwaps.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minSwap(vector<int>& nums1, vector<int>& nums2) {
+        
+        int ans = 0;
+
+        for(int i= 0; i<nums1.size(); i++){
+ 		
+        }
+    }
+};
